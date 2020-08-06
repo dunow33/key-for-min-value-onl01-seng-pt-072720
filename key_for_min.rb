354b7e2ea0253b899c3976fb78880229c7e54ae7
Brian Donnoe <dunow33@hotmail.com>
2020-08-06 05:24:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,13 @@
 def key_for_min_value(name_hash)
   small = 0
   
+  counter = 1
   name_hash.each do | key, value |
-    if value 
+    if counter == 1
+      if small != value
+        small = value
+      end
+    end
     if value < small
       small = value
       puts value
